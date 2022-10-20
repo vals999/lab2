140210e5f8903638831e8c6d9baf4dcbf1ee0ec7
vals999 <valentinsantana2010@hotmail.com>
2022-10-20 18:34:43 +0000

    Actualizar modelos monster y victim

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,5 @@
 class Monsters < ApplicationRecord
     validates :name, presence: true
     validates :name, uniqueness: true
+    has_many : victims
 end
--- app/models/victim.rb
+++ app/models/victim.rb
@@ -1,3 +1,4 @@
 class Victims < ApplicationRecord
     validates :name, presence: true
+    belongs_to :monster
 end
